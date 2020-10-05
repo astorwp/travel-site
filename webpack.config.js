0883811cfab12e3c985550f18ae1f6fcc1f85eaa
Mike <mike@astorwebpro.com>
2020-10-05 16:46:02 -0500

    Added post css a different way from the videos which seems to work better; reference S4 L5

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,25 @@ module.exports = {
     watch: true,
     module: {
         rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader','css-loader']
-            }
+          {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: [
+              {
+                loader: 'style-loader',
+              },
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
+                }
+              },
+              {
+                loader: 'postcss-loader'
+              }
+            ]
+          }
         ]
-    }
+      }
+    
 }
