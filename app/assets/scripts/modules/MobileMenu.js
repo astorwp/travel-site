6be793db8f17e9cf21613ec0a9e940bddfc012e1
Mike <mike@astorwebpro.com>
2020-10-08 12:26:14 -0500

    Add buttons to header menu for style

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,6 +2,7 @@ class MobileMenu {
     constructor() {
         this.menuIcon = document.querySelector(".site-header__menu-icon")
         this.menuContent = document.querySelector(".site-header__menu-content")
+        this.sitHeader = document.querySelector(".site-header")
         this.events()
     }
 
@@ -11,6 +12,7 @@ class MobileMenu {
 
     toggleTheMenu() {
         this.menuContent.classList.toggle("site-header__menu-content--is-visible")
+        this.sitHeader.classList.toggle("site-header--is-expanded")
     }
 }
 
